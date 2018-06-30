991890e4fdce76a65dd746b4cde807b6da38569d
Chen Li <498526554@qq.com>
2018-06-29 23:18:11 -0400

    add toolbar

--- assignments/a3/src/main/java/ToolBar.java
+++ assignments/a3/src/main/java/ToolBar.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -19,20 +20,34 @@ public class ToolBar extends JPanel implements Observer {
     private void config_button() {
         grid_layout.setIcon(new ImageIcon("images/grid.png"));
         list_layout.setIcon(new ImageIcon("images/list.png"));
+        fotlag.setFont(new Font("Dialog", 1, 25));
         load.setIcon(new ImageIcon("images/load.png"));
+        filter_by.setFont(new Font("Dialog", 1, 15));
         clear_filter.setIcon(new ImageIcon("images/empty.png"));
     }
 
     ToolBar(ImageCollectionModel imagecollectionmodel) {
         this.imagecollectionmodel = imagecollectionmodel;
 
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
         config_button();
         //set_button_listener();
-
-        this.add(grid_layout);
+/*
         this.add(list_layout);
         this.add(fotlag);
         this.add(load);
+        this.add(filter_by);
+        this.add(clear_filter); */
+
+
+        this.add(grid_layout);
+        this.add(Box.createRigidArea(new Dimension(10,0)));
+        this.add(list_layout);
+        this.add(fotlag);
+        this.add(Box.createHorizontalGlue());
+        this.add(load);
+        this.add(Box.createHorizontalStrut(10));
         this.add(filter_by);
         this.add(clear_filter);
     }
