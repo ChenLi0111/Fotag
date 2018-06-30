7a58a747332204f1c9601ab601103c2c51829958
Chen Li <498526554@qq.com>
2018-06-29 23:27:04 -0400

    button size

--- assignments/a3/src/main/java/ToolBar.java
+++ assignments/a3/src/main/java/ToolBar.java
@@ -19,11 +19,20 @@ public class ToolBar extends JPanel implements Observer {
 
     private void config_button() {
         grid_layout.setIcon(new ImageIcon("images/grid.png"));
+        grid_layout.setSize(new Dimension(55, 55));
+
         list_layout.setIcon(new ImageIcon("images/list.png"));
+        list_layout.setSize(new Dimension(55, 55));
+
         fotlag.setFont(new Font("Dialog", 1, 25));
+
         load.setIcon(new ImageIcon("images/load.png"));
+        load.setSize(new Dimension(55, 55));
+
         filter_by.setFont(new Font("Dialog", 1, 15));
+
         clear_filter.setIcon(new ImageIcon("images/empty.png"));
+        clear_filter.setSize(new Dimension(55, 55));
     }
 
     ToolBar(ImageCollectionModel imagecollectionmodel) {
@@ -32,14 +41,6 @@ public class ToolBar extends JPanel implements Observer {
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
         config_button();
-        //set_button_listener();
-/*
-        this.add(list_layout);
-        this.add(fotlag);
-        this.add(load);
-        this.add(filter_by);
-        this.add(clear_filter); */
-
 
         this.add(grid_layout);
         this.add(Box.createRigidArea(new Dimension(10,0)));
